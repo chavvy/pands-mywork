42f3b65ea62cafe34c74de5bb22a78c70f4314b0
chavvy <samedo1999@gmail.com>
2025-02-08 11:28:47 +0000

    week 02 work

---
+++ week02/extra/nameAndAge.py
@@ -0,0 +1,9 @@
+#lab 2.2 extra
+#A program that will read the users input for Name and Age, and give an output using them
+
+#User input for name and age
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+#Output
+print (f"Hello {name},\tyour age is {age}.")
