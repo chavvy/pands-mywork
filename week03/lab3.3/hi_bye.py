d6ff8cfc136fc0ce9d12c3f16cfeb05642eb3d87
chavvy <samedo1999@gmail.com>
2025-04-25 02:39:32 +0100

    topic 3 labs

---
+++ week03/lab3.3/hi_bye.py
@@ -0,0 +1,7 @@
+#lab 3.3
+#formatting dialogue
+#\t for a tabs length of space, \n for a new line
+
+dialogue = ("John said\t'Hi'\nSam said\t'Bye'")
+
+print(dialogue)
---
+++ week03/lab3.3/length.py
@@ -0,0 +1,9 @@
+#lab 3.3
+#a program that reads in a string and outputs how long it is
+
+string_input = input("Please enter a string: ")
+count_output = len(string_input)
+
+#printing the result
+
+print("The length of {} is {}".format(string_input, count_output))
